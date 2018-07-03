b46492131b4c0fbdae691d1ac0ac657759980de8
zinmyintnaung <zinmyintnaung@gmail.com>
2018-07-03 16:42:02 +0800

    ch6 mod

--- 06-ajax-fetch/10-fetch-http-lib/easyhttp2.js
+++ 06-ajax-fetch/10-fetch-http-lib/easyhttp2.js
@@ -3,7 +3,7 @@
  * Library for making HTTP requests
  *
  * @version 2.0.0
- * @author  Brad Traversy
+ * @author  
  * @license MIT
  *
  **/
--- 06-ajax-fetch/12-http-lib-async/easyhttp3.js
+++ 06-ajax-fetch/12-http-lib-async/easyhttp3.js
@@ -3,7 +3,7 @@
  * Library for making HTTP requests
  *
  * @version 3.0.0
- * @author  Brad Traversy
+ * @author  
  * @license MIT
  *
  **/
